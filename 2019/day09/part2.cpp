02320e9863032822583cd9b66c8c6b0a3bb547a6
Tyler Woodhull <tyler.woodhull@gmail.com>
2019-12-11 15:27:13 -0500

    Day 9 Solution

---
+++ 2019/day09/part2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "IntCodeMachine.h"
+
+
+int main(int argc, char** argv) {
+    IntCodeMachine machine = IntCodeMachine::loadMachineFromFile(argv[1]);
+    machine.queueInput(2);
+    machine.runProgram();
+
+    while (machine.hasOutput()) {
+        cout << machine.readNextOutput() << endl;
+    }
+
+    return 0;
+}
